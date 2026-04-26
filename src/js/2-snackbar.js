282f175a9a575046441ee32efecc03d3bed094cc
Maxym Yurakh <maksaaasssdfg@gmail.com>
2026-04-26 11:58:54 +0300

    task 2 done

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,1 +1,46 @@
 'use strict';
+
+import iziToast from "izitoast";
+
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector('.form');
+
+const onSubmit = (event) => {
+    event.preventDefault();
+    const currForm = event.currentTarget;
+
+    const delay = Number(currForm.elements.delay.value);
+    const state = currForm.elements.state.value;
+
+    createPromise(delay, state)
+        .then((delayValue) => {
+            iziToast.success({
+                message: `✅ Fulfilled promise in ${delayValue}ms`,
+                position: "topRight",
+                icon: ''
+            });
+        })
+        .catch((delayValue) => {
+            iziToast.error({
+                message: `❌ Rejected promise in ${delayValue}ms`,
+                position: "topRight",
+                icon: ''
+            });
+        });
+    currForm.reset();
+};
+
+form.addEventListener('submit', onSubmit);
+
+const createPromise = (delay, state) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (state === 'fulfilled') {
+                resolve(delay);
+            } else {
+                reject(delay);
+            }
+        }, delay);
+    })
+}
